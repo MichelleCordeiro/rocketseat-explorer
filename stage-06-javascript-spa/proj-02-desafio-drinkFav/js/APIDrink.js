1d51af08e36c8965fb53091237e826fbaefa1300
MichelleCordeiro <mic321@gmail.com>
2023-08-23 03:52:13 -0300

    83: proj 02 challenge - update to drinkfav and use drinks api

---
+++ stage-06-javascript-spa/proj-02-desafio-drinkFav/js/APIDrink.js
@@ -0,0 +1,21 @@
+export class APIDrink {
+  static search(drinkname) {
+    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkname}`;
+
+    return fetch(endpoint)
+      .then(data => data.json())
+      .then(data => data.drinks[0])
+      .then(data => ({
+        image: data.strDrinkThumb,
+        name: data.strDrink,
+        category: data.strCategory,
+        served_in: data.strGlass,
+        ingredient1: data.strIngredient1,
+        ingredient2: data.strIngredient2,
+        ingredient3: data.strIngredient3,
+        ingredient4: data.strIngredient4,
+        ingredient5: data.strIngredient5,
+        ingredient6: data.strIngredient6
+      }));
+  }
+}
---
+++ stage-06-javascript-spa/proj-02-desafio-drinkFav/js/Favorites.js
@@ -0,0 +1,157 @@
+import { APIDrink } from './APIDrink.js';
+
+// manipulação dos dados
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root);
+    this.load();
+    APIDrink.search('Mojito').then(item => console.log('APIDrink: ', item));
+    // APIDrink.search('Mojito').then(item => console.log('APIDrink: ', item.drinks[0]));
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@drinks-favorites:')) || [];
+  }
+
+  save() {
+    localStorage.setItem('@drinks-favorites:', JSON.stringify(this.entries))
+    console.log('this.entries SAVE: ', this.entries);
+  }
+
+  async add(drinkname) {
+    try {
+      // const drinkExists = this.entries.find(entry => entry.name === drinkname);
+      // console.log('entry.name ADD: ', entry.name);
+      // console.log('this.entries ADD----: ', this.entries);
+      // console.log('drinkExists: ', drinkExists);      
+      
+      // if (drinkExists) {
+      //   throw new Error('Drink já cadastrado!');
+      // }
+        
+      // const apidrinks = await APIDrink.search(drinkname);
+      // const drinksearch = apidrinks.drinks[0];
+      const drinksearch = await APIDrink.search(drinkname);
+  
+
+      if (drinksearch === undefined) {
+        throw new Error('Drink não encontrado!');
+      }
+
+      this.entries = [drinksearch, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch (error) {
+      alert(error.message)
+    }
+  }s
+
+  delete(drinkname) {
+    const filteredEntries = this.entries.filter(x => x.name !== drinkname.name);
+      console.log('this.entries ADD----: ', this.entries);
+    console.log('x.name ADD: ', x.name);
+
+    // filter só retorna se for true, retornará users diferentes ao clicado p excluir
+
+    this.entries = filteredEntries;
+    this.update();
+    this.save();
+  }
+}
+
+// exibição dos dados
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root);
+
+    this.tbody = this.root.querySelector('table tbody');
+
+    this.update();
+    this.onadd()
+  }
+
+  update() {
+    this.removeTrs();
+    const clearInput = document.querySelector('#input-search')
+    clearInput.value = ''
+    
+    this.entries.forEach(drink => {
+      const row = this.createRow();
+      
+      const ingredients = [drink.ingredient1, drink.ingredient2, drink.ingredient3, drink.ingredient4, drink.ingredient5];
+      let ingredientsPresents = ingredients.filter(ing => ing !== null)
+      ingredientsPresents = ingredientsPresents.join(' + ');
+
+      row.querySelector('.drink img').src = drink.image;
+      row.querySelector('.drink img').alt = `Imagem de ${drink.name}`;
+      row.querySelector('.drink a').href = `https://github.com/${drink.name}`;
+      row.querySelector('.drink a').href = `#`;
+      row.querySelector('.drink .container-drink p').textContent = drink.name;
+      row.querySelector('.drink .container-drink span').textContent = ingredientsPresents;
+      row.querySelector('.category').textContent = drink.category;
+      row.querySelector('.served_in').textContent = drink.served_in;
+
+      row.querySelector('.remove-btn').onclick = () => {
+        const isOK = confirm('Tem certeza que deseja remover esse drink?');
+
+        if (isOK) {
+          this.delete(row);
+        }
+      };
+
+      this.tbody.append(row);
+    });
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('#btn-fav')
+    const inputSearch = this.root.querySelector('#input-search');
+
+    addButton.onclick = () => {
+      const { value } = inputSearch;
+      this.add(value)
+    }
+
+    inputSearch.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        const { value } = inputSearch;
+        this.add(value)
+      }
+    })
+  }
+
+  createRow() {
+    // precisa criar o tr pela dom
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+      <td class="drink">
+        <a href="#" target="_blank">
+          <div class="container-img">
+            <img src="https://www.thecocktaildb.com/images/media/drink/metwgh1606770327.jpg" alt="Foto de um Morito">
+          </div>
+          <div class="container-drink">
+            <p>Mojito</p>
+            <span>Light run, juice of 1 lime</span>
+          </div>
+        </a>
+      </td>
+      <td class="category">33</td>
+      <td class="served_in">333</td>
+      <td class="remove">
+        <button class="remove-btn">
+          <img src="./assets/not_drink.svg" alt="Desenho proibido drink" width="20" height="20">
+        </button>
+      </td>
+    `;
+
+    return tr;
+  }
+
+  removeTrs() {
+    this.tbody.querySelectorAll('tr').forEach(tr => {
+      tr.remove();
+    });
+  }
+}
