b402880716937c4ce2d0718f8c8605abf8e898b1
MichelleCordeiro <mic321@gmail.com>
2023-09-12 19:00:09 -0300

    96: backend api - add notes routes

--- stage-08-backend/api/src/database/knex/index.js
+++ stage-08-backend/api/src/database/knex/index.js
@@ -1,6 +1,6 @@
 const config = require('../../../knexfile')
 const knex = require('knex')
 
-const connection = knex.Knex(config.development)
+const connection = knex(config.development)
 
 module.exports = connection
