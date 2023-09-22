61323719caacafbe514e78d0cfd411cba739bbbf
MichelleCordeiro <mic321@gmail.com>
2023-09-22 19:41:14 -0300

    118: desafio rocketMovies - add profile page

--- stage-09-frontend/desafio-movie/rocketmovies/src/main.jsx
+++ stage-09-frontend/desafio-movie/rocketmovies/src/main.jsx
@@ -6,13 +6,14 @@ import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
 import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp';
+import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
-);
+)
---
+++ stage-09-frontend/desafio-movie/rocketmovies/src/pages/Profile/styles.js
@@ -0,0 +1,80 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+
+  > header {
+    width: 100%;
+    height: 14.4rem;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_home_profile};
+    
+    > a {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      
+      padding: 6.4rem 14.6rem 6rem;
+      color: ${({ theme }) => theme.COLORS.PINK};
+    }
+  }
+`
+
+export const Form = styled.form`
+  max-width: 34rem;
+  margin: 0 auto 3rem;
+  gap: 0.8rem;
+
+  :nth-of-type(3) {
+    margin-bottom: 2.4rem;
+  }
+
+  > img {
+    width: 18.6rem;
+    height: 18.6rem;
+    border-radius: 50%;
+  }
+`
+
+export const Avatar = styled.div`
+  width: 18.6rem;
+  height: 18.6rem;
+  margin: -9rem auto 6.4rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: relative;
+
+  > img {
+    width: 18.6rem;
+    height: 18.6rem;
+    border-radius: 50%;
+  }
+
+  > label {
+    width: 4.8rem;
+    height: 4.8rem;
+    background-color: ${({ theme }) => theme.COLORS.PINK};
+    border-radius: 50%;
+
+    input {
+      display: none;
+    }
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+
+    bottom: 7px;
+    right: 7px;
+
+    cursor: pointer;
+  }
+
+  svg {
+    color: ${({ theme }) => theme.COLORS.BACKGROUND_800_input};
+  }
+`
