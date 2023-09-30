fb22a0670bb75dbf593fb3ebbfcdd031635e4122
MichelleCordeiro <mic321@gmail.com>
2023-09-30 01:36:12 -0300

    131: api restful - save image in db

--- stage-10-api-restful/api/src/routes/users.routes.js
+++ stage-10-api-restful/api/src/routes/users.routes.js
@@ -3,18 +3,22 @@ const multer = require('multer')
 const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (request, response) => {
-  console.log(request.file.filename)
-  response.json()
-})
+usersRoutes.patch(
+  '/avatar', 
+  ensureAuthenticated, 
+  upload.single('avatar'), 
+  userAvatarController.update
+)
 
 module.exports = usersRoutes
