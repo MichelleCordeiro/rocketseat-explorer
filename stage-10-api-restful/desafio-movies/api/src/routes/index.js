a3a31f0a71e5392633f444d65967734e1ae0d18a
MichelleCordeiro <mic321@gmail.com>
2023-10-01 12:29:36 -0300

    133: add Stage 10 - desafio movies api restful

---
+++ stage-10-api-restful/desafio-movies/api/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+
+const usersRoutes = require('./users.routes')
+const notesRoutes = require('./notes.routes')
+const tagsRoutes = require('./tags.routes')
+const sessionsRoutes = require('./sessions.routes')
+
+const routes = Router()
+routes.use('/users', usersRoutes)
+routes.use('/sessions', sessionsRoutes)
+routes.use('/notes', notesRoutes)
+routes.use('/tags', tagsRoutes)
+
+module.exports = routes
