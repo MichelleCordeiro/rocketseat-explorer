900078bf5053f670dedf180c4e6d9a1ef41b24f1
MichelleCordeiro <mic321@gmail.com>
2023-10-01 12:47:11 -0300

    134: add desafio movies restful - validate user and password

--- stage-10-api-restful/desafio-movies/api/src/controllers/SessionsController.js
+++ stage-10-api-restful/desafio-movies/api/src/controllers/SessionsController.js
@@ -1,8 +1,24 @@
+const knex = require('../database/knex')
+const AppError = require('../utils/AppError.js')
+const { compare } = require('bcryptjs')
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body
 
-    return response.json({ email, password })
+    const user = await knex('users').where({ email }).first()
+
+    if (!user) {
+      throw new AppError('E-mail e/ou senha incorreta', 401)
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new AppError('E-mail e/ou senha incorreta', 401)
+    }
+
+    return response.json(user)
   }
 }
 
