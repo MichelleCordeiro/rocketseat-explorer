b9b48da4019f0e162b1f5914616c6e095966bd0a
MichelleCordeiro <mic321@gmail.com>
2023-10-02 20:50:55 -0300

    135: desafio movies restful - generate user token jwt

--- stage-10-api-restful/desafio-movies/api/src/controllers/SessionsController.js
+++ stage-10-api-restful/desafio-movies/api/src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError.js')
 const { compare } = require('bcryptjs')
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
 
 class SessionsController {
   async create(request, response) {
@@ -18,7 +20,13 @@ class SessionsController {
       throw new AppError('E-mail e/ou senha incorreta', 401)
     }
 
-    return response.json(user)
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token })
   }
 }
 
