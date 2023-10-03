89742948135df065c2ab1b11bc60e8252cc66301
MichelleCordeiro <mic321@gmail.com>
2023-10-02 23:52:41 -0300

    139: desafio movies restful - disk storage and upload img

---
+++ stage-10-api-restful/desafio-movies/api/src/providers/DiskStorage.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+const uploadConfig = require('../configs/upload')
+
+class DiskUpload {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.TMP_FOLDER, file),
+      path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    )
+
+    return file
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+
+    try {
+      await fs.promises.stat(filePath)
+    } catch (error) {
+      alert(error.message)
+    }
+
+    await fs.promises.unlink(file)
+  }
+}
+
+module.exports = DiskUpload
