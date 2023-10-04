48d9779774c1d08877d50d654c9083bcace04445
MichelleCordeiro <mic321@gmail.com>
2023-10-04 14:37:00 -0300

    148: rocketnotes - add local storage for user and token

--- stage-09-frontend/rocketnotes/src/hooks/auth.jsx
+++ stage-09-frontend/rocketnotes/src/hooks/auth.jsx
@@ -1,5 +1,6 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import { api } from '../services/api'
+import { json } from 'react-router-dom'
 
 export const AuthContext = createContext({})
 
@@ -10,6 +11,9 @@ function AuthProvider({ children }) {
     try {
       const response = await api.post('/sessions', { email, password})
       const { user, token } = response.data
+
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+      localStorage.setItem('@rocketnotes:token', token)
 
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({ user, token })
@@ -22,6 +26,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@rocketnotes:token')
+    const user = localStorage.getItem('@rocketnotes:user')
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, [])
   
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
