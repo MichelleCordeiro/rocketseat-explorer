5eeb8481459f56dea6aaf6463c6191b156ef3760
MichelleCordeiro <mic321@gmail.com>
2023-10-07 01:30:28 -0300

    158: rocketnotes - open note

--- stage-09-frontend/rocketnotes/src/pages/Home/index.jsx
+++ stage-09-frontend/rocketnotes/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
 
 import { api } from '../../services/api'
@@ -17,6 +18,8 @@ export function Home() {
   const [tagsSelected, setTagsSelected] = useState([])
   const [notes, setNotes] = useState([])
 
+  const navigate = useNavigate()
+
   function handleTagsSelected(tagName) {
     if (tagName === 'Todos') {
       return setTagsSelected([])
@@ -34,6 +37,10 @@ export function Home() {
     } else {
       setTagsSelected(prevState => [...prevState, tagName])
     }
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`)
   }
 
   useEffect(() => {
@@ -92,6 +99,7 @@ export function Home() {
               <Note
                 key={String(note.id)}
                 data={note}
+                onClick={() => handleDetails(note.id)}
               />
             ))
           }
