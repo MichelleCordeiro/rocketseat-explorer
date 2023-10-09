1d205fc5af0a1eeced475a8f5615b30c44dc8f64
MichelleCordeiro <mic321@gmail.com>
2023-10-09 01:19:29 -0300

    171: desafio movies and api - update user avatar

--- stage-09-frontend/desafio-movie/rocketmovies/src/components/Header/index.jsx
+++ stage-09-frontend/desafio-movie/rocketmovies/src/components/Header/index.jsx
@@ -1,10 +1,17 @@
 import { useAuth } from '../../hooks/auth'
 
+import { api } from '../../services/api'
+
 import { Input } from '../Input'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 import { Container, Brand, Profile, Logout } from './styles'
 
 export function Header() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder
 
   return (
     <Container>
@@ -15,9 +22,9 @@ export function Header() {
       <Input placeholder='Pesquisar pelo título' type='text' />
 
       <Profile to='/profile'>
-        <strong>Michelle Cordeiro</strong>
+        <strong>{user.name}</strong>
 
-        <img src='http://github.com/michellecordeiro.png' alt='' />
+        <img src={avatarUrl} alt={user.name} />
       </Profile>
 
       <Logout onClick={signOut}>
--- stage-09-frontend/desafio-movie/rocketmovies/src/pages/Profile/index.jsx
+++ stage-09-frontend/desafio-movie/rocketmovies/src/pages/Profile/index.jsx
@@ -4,29 +4,49 @@ import { Link } from 'react-router-dom'
 
 import { useAuth } from '../../hooks/auth'
 
+import { api } from '../../services/api'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { Container, Form, Avatar } from './styles'
 
 export function Profile() {
   const { user, updateProfile } = useAuth()
-
+  
   const [name, setName] = useState(user.name)
   const [email, setEmail] = useState(user.email)
   const [passwordOld, setPasswordOld] = useState()
   const [passwordNew, setPasswordNew] = useState()
+  
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder
+  
+  const [avatar, setAvatar] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState(null)
 
   async function handleUpdate() {
-    const user = {
+    const updated = {
       name,
       email,
       password: passwordNew,
       old_password: passwordOld
     }
 
-    await updateProfile({ user })
+    const userUpdated = Object.assign(user, updated)
+
+    await updateProfile({ user: userUpdated, avatarFile })
   }
 
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0]
+    setAvatarFile(file)
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
+  }
+  
   return (
     <Container>
       <header>
@@ -38,11 +58,16 @@ export function Profile() {
 
       <Form>
         <Avatar>
-          <img src='https://github.com/michellecordeiro.png' alt='Foto do usuário' />
+          <img src={avatar} alt='Foto do usuário' />
 
           <label htmlFor='avatar'>
             <FiCamera />
-            <input id='avatar' type='file' />
+
+            <input 
+              id='avatar' 
+              type='file' 
+              onChange={handleChangeAvatar} 
+            />
           </label>
         </Avatar>
 
