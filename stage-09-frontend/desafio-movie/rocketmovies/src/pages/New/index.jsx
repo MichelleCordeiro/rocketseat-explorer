4917a3433f49ceb40e16fe363e55d2ed6b577045
MichelleCordeiro <mic321@gmail.com>
2023-10-09 14:18:34 -0300

    173: desafio movies - register and validate notes

--- stage-09-frontend/desafio-movie/rocketmovies/src/pages/New/index.jsx
+++ stage-09-frontend/desafio-movie/rocketmovies/src/pages/New/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
+import { useNavigate } from 'react-router-dom'
+
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
 import { Input } from '../../components/Input'
@@ -9,11 +11,19 @@ import { Textarea } from '../../components/Textarea'
 import { Button } from '../../components/Button'
 import { MovieItem } from '../../components/MovieItem'
 
+import { api } from '../../services/api'
+
 import { Container, Form } from './styles'
 
 export function New() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  let [rating, setRating] = useState('')
+
   const [tags, setTags] = useState([])
   const [newTag, setNewTag] = useState('')
+
+  const navigate = useNavigate()
 
   function handleAddTag() {
     setTags(prevState => [...prevState, newTag])
@@ -23,6 +33,37 @@ export function New() {
   function handleRemoveTag(deleted) {
     setTags(prevState => prevState.filter(tag => tag !== deleted))
   }
+
+  async function handleNewNote() {
+    if (!title) {
+      return alert('Digite o título do filme')
+    }
+
+    if (!rating) {
+      rating = 0
+    }
+    
+    if (newTag) {
+      return alert(
+        'Você digitou uma tag, mas não clicou para adicioná-la. Clique para adicionar ou deixe o campo vazio.'
+      )
+    }
+
+    if (tags.length < 1) {
+      return alert('Adicione pelo menos uma tag')
+    }
+
+    await api.post('/notes', {
+      title,
+      rating,
+      description,
+      tags
+    })
+
+    alert('Filme adicionado com sucesso!')
+    navigate('/')
+  }
+
   return (
     <Container>
       <Header />
@@ -37,22 +78,26 @@ export function New() {
           <Section title='Novo filme'></Section>
 
           <div>
-            <Input placeholder='Título' type='text' />
-            <Input placeholder='Sua nota (de 0 a 5)' type='text' />
+            <Input placeholder='Título' type='text' onChange={e => setTitle(e.target.value)} />
+
+            <Input
+              placeholder='Sua nota (de 0 a 5)'
+              type='text'
+              onChange={e => setRating(e.target.value)}
+            />
           </div>
-          <Textarea placeholder='Observações' type='text' />
+
+          <Textarea
+            placeholder='Observações'
+            type='text'
+            onChange={e => setDescription(e.target.value)}
+          />
 
           <h3>Marcadores</h3>
           <div className='tags'>
-            {
-              tags.map((tag, index) => (
-                <MovieItem 
-                    key={String(index)}
-                    value={tag}
-                    onClick={( ) => handleRemoveTag(tag)}
-                  />
-              ))
-            }
+            {tags.map((tag, index) => (
+              <MovieItem key={String(index)} value={tag} onClick={() => handleRemoveTag(tag)} />
+            ))}
             <MovieItem
               isNew
               placeholder='Nova tag'
@@ -64,7 +109,7 @@ export function New() {
 
           <div className='btns'>
             <Button title='Excluir filme' />
-            <Button title='Salvar alterações' />
+            <Button title='Salvar alterações' onClick={handleNewNote} />
           </div>
         </Form>
       </main>
